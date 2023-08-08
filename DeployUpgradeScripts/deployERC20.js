11f38225b454e6ece0e2e864db62f7a3fcc8dcd7
wasim162010 <47940538+wasim162010@users.noreply.github.com>
2023-08-08 04:07:31 -0700

    Update deployERC20.js

--- DeployUpgradeScripts/deployERC20.js
+++ DeployUpgradeScripts/deployERC20.js
@@ -1,1 +1,14 @@
 
+
+const {ethers,upgrades} = require("hardhat");
+
+async function main() {
+
+  const TokenERC20 = await ethers.getContractFactory("TokenERC20");
+  console.log("Deploying");
+  const erc20= await upgrades.deployProxy(TokenERC20,  {initializer: "initVars"});
+  await erc20.deployed()
+  console.log("deployed TokenERC20 ", erc20.address);
+}
+
+main();
